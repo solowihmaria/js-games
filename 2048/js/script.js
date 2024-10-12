02458357bcf56be0375a1f73b935b5dfef26ef98
solowihmaria <solowihmaria@gmail.com>
2024-10-12 16:54:48 +0300

    feat: start adding functions

---
+++ 2048/js/script.js
@@ -0,0 +1,40 @@
+let board;
+let score = 0;
+let rows = 4;
+let columns = 4;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for (let r = 0; r < rows; r++) {
+        for(let c = 0; r < columns; c++){
+            //<div id="0-0"></div>
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c];
+            updateTile(tile, num);
+
+        }
+    }
+}
+
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = "";
+    tile.classList.add("tile");
+    if (num > 0) {
+        tile.innerText = num;
+        if (num <= 4096) {
+            
+        }
+    }
+}
